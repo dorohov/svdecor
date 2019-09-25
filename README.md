fe2efb0302f643f0e729df3b8deba795d20d4e3b
Pavel Proyavin <pavel@dorohovdesign.com>
2019-09-25 17:54:28 +0300

    changelog from 250919

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SVDECOR
+
+### В шаблоне есть модалки, вызываются они так:
+
+```javascript
+openModal(modalID);
+```
+
+#### Options
+
+- ModalID - Идентификатор модального окна
+
+#### IDs:
+
+- #modal_form - Форма
---
+++ src/js/modals.js
@@ -0,0 +1,22 @@
+function openModal(modalID) {
+    $(modalID).modal({
+        fadeDuration: 100,
+        showClose: false,
+    })
+}
+
+(function($) {
+    "use strict"
+    $(function() {
+
+        $('a.modal-open').on('click', function(e) {
+            e.preventDefault()
+            var thisModalId = $(this).attr('href')
+            openModal(thisModalId)
+            return false;
+        })
+
+        // openModal('#modal_form')
+
+    })
+})(jQuery);
