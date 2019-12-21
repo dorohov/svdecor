d2a16439c010312975d4e21a322cd7955e5b192f
Pavel Proyavin <pavel@dorohovdesign.com>
2019-12-21 13:40:19 +0300

    fx f 211219

--- src/js/basket.js
+++ src/js/basket.js
@@ -18,21 +18,24 @@
 
             $('.is--del-form').show();
 
-        })  
+        })
 
-        // var ourWidjet = new ISDEKWidjet ({
-        //     defaultCity: 'Москва',
-        //     cityFrom: 'Москва',
-        //     country: 'Россия',
-        //     link: 'modal-map__inner',
-        //     path: 'https://www.cdek.ru/website/edostavka/template/scripts/',
-        //     servicepath: 'http://frontend.dorohovdesign.ru/svdecor/service.php',
-        //     hidedelt: true,
-        //     onChoose: function(wat) {
-        //         $('#f_delivery-a').val(wat.cityName + ', ' + wat.PVZ.Address);
-        //         $.modal.close();
-        //     }
-        // });
+        console.log(location.origin)
+
+        var ourWidjet = new ISDEKWidjet ({
+            defaultCity: 'Москва',
+            cityFrom: 'Москва',
+            country: 'Россия',
+            link: 'modal-map__inner',
+            apikey: '127e0ca6-e6ff-462b-96c2-3e62bd5580df',
+            path: sitepath + 'dist/widget/scripts/',
+            servicepath: sitepath + 'dist/widget/scripts/service.php',
+            hidedelt: true,
+            onChoose: function(wat) {
+                $('#f_delivery-a').val(wat.cityName + ', ' + wat.PVZ.Address);
+                $.modal.close();
+            }
+        });
 
         $('.modal__picker__block__inner').on('click', function() {
             var thisColor = $(this).data('color')
